1b615b8116e1599777f07b06218c27bff3c98c2b
[Maureen Bayne] <[maureenbayne34@gmail.com]>
2023-12-12 13:28:48 -0700

    adding project

---
+++ main.py
@@ -0,0 +1,158 @@
+from tkinter import *
+from word_list import word_list
+import random
+
+# ---------------------------- CONSTANTS ------------------------------- #
+BLACK = "#000000"
+WHITE = "#ffffff"
+RED = "#ff0000"
+FONT_NAME = "Courier"
+ENTRIES = []
+INCORRECT_WORDS = []
+TIMER_RUNNING = False
+TIME_ELAPSED = 60
+IS_PLAYING = True
+
+
+# ---------------------------- TRACKING ENTRY ------------------------------- #
+# TODO: Fix repeating words
+def update_entry(user_input, word_label, entry_widget):
+    global TIME_ELAPSED
+
+    # new_word = random.choice(word_list)
+
+    # Choose a word that has not been used yet
+    unused_words = set(word_list) - set(ENTRIES)
+    new_word = random.choice(list(unused_words))
+
+    if TIME_ELAPSED > 0:
+        word_label.config(text=new_word, font=(FONT_NAME, 50, "bold"), bg=BLACK, fg=WHITE)
+        word_label.place(x=275, y=125)
+        entry_widget.delete(0, END)
+
+        # Need to remove space in list
+        entry = user_input.strip()
+
+        if entry in word_list:
+            ENTRIES.append(entry)
+        else:
+            if entry != "":
+                INCORRECT_WORDS.append(entry)
+    else:
+        # Update word label
+        word_label.config(text="GAME OVER")
+        word_to_type_label.place(x=200, y=125)
+
+        # Update entry widget
+        entry_widget.delete(0, END)
+        entry_widget.insert(0, "Click 'Results'")
+
+
+# # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer(*args):
+    global TIMER_RUNNING
+    if not TIMER_RUNNING:
+        TIMER_RUNNING = True
+        update_timer()
+
+
+def update_timer():
+    global TIME_ELAPSED
+    if TIMER_RUNNING:
+        if TIME_ELAPSED > 9:
+            timer_label.config(text=f"00:{TIME_ELAPSED}")
+            window.after(1000, update_timer)  # Update every 1000 milliseconds (1 second)
+            TIME_ELAPSED -= 1
+        elif TIME_ELAPSED > 0:
+            timer_label.config(text=f"00:0{TIME_ELAPSED}")
+            window.after(1000, update_timer)
+            TIME_ELAPSED -= 1
+        else:
+            timer_label.config(text=f"00:00 TIME'S UP!", fg=RED)
+            timer_label.place(x=200, y=70)
+
+
+# ---------------------------- RESET GAME ------------------------------- #
+def reset_game():
+    global ENTRIES, INCORRECT_WORDS, TIMER_RUNNING, TIME_ELAPSED
+
+    # Clear previous game data
+    ENTRIES = []
+    INCORRECT_WORDS = []
+    TIMER_RUNNING = False
+    TIME_ELAPSED = 60
+
+    # Reset labels
+    timer_label.config(text="01:00", fg=WHITE)
+    word_to_type_label.config(text="Press 'Space' to start.", font=(FONT_NAME, 30, "bold"), bg=BLACK, fg=WHITE)
+    word_to_type_label.place(x=150, y=125)
+
+    # Clear entry
+    typing_entry.delete(0, END)
+
+    # Reset timer label position
+    timer_label.place(x=330, y=70)
+
+    # Destroy result labels if they exist
+    for widget in window.winfo_children():
+        if isinstance(widget, Label) and "results" in widget.cget("text").lower():
+            widget.destroy()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Typing Speed Test")
+window.config(padx=100, pady=50, bg=BLACK)
+
+# Set the position of the window on the screen. Set for 1920 x 1080 monitor
+window_x = 400
+window_y = 50
+window.geometry(f"+{window_x}+{window_y}")
+
+# Canvas
+canvas = Canvas(width=800, height=824, bg=BLACK, highlightthickness=0)
+typewriter_img = PhotoImage(file="images/typewriter_medium.png")
+canvas.create_image(380, 475, image=typewriter_img)
+canvas.grid(column=1, row=1)
+
+# Labels
+instructions = Label(text="You have 60 seconds to type as many words as possible. Use 'SPACE BAR' for next word. "
+                          "Start typing to start timer.",
+                     font=(FONT_NAME, 18, "bold"), bg=BLACK, fg=WHITE, wraplength=900)
+instructions.place(x=30, y=0)
+
+timer_label = Label(text="01:00", font=(FONT_NAME, 35, "bold"), bg=BLACK, fg=WHITE)
+timer_label.place(x=330, y=70)
+
+word_to_type_label = Label(text="Press 'Space' to start.", font=(FONT_NAME, 30, "bold"), bg=BLACK, fg=WHITE)
+word_to_type_label.place(x=150, y=125)
+
+# Entries
+typing_entry = Entry(width=80)
+typing_entry.place(x=150, y=215)
+typing_entry.focus()
+typing_entry.bind("<space>", (lambda event: update_entry(typing_entry.get(), word_to_type_label, typing_entry)))
+typing_entry.bind("<Key>", start_timer)
+
+# Buttons
+reset_button = Button(text="Try Again", command=reset_game)
+reset_button.place(x=705, y=212)
+
+
+# ---------------------------- RESULTS ------------------------------- #
+def see_results():
+    # Result Labels
+    correct_results = Label(text=f"Results: {len(ENTRIES)} words per minute.",
+                            font=(FONT_NAME, 18, "bold"), bg=BLACK, fg=WHITE, wraplength=800)
+    correct_results.place(x=200, y=725)
+
+    incorrect_results = Label(text=f"Misspelled results: {INCORRECT_WORDS}",
+                              font=(FONT_NAME, 18, "bold"), bg=BLACK, fg=WHITE, wraplength=800)
+    incorrect_results.place(x=30, y=775)
+
+
+# Button to print entries
+results_button = Button(text="Results", command=see_results)
+results_button.place(x=650, y=212)
+
+window.mainloop()
---
+++ word_list.py
@@ -0,0 +1,24 @@
+word_list = ['apple', 'grape', 'lemon', 'peach', 'melon', 'kiwi', 'cherry', 'plum', 'banana', 'pear',
+             'cloud', 'rainy', 'sunny', 'storm', 'windy', 'foggy', 'snowy', 'clear', 'night', 'morning',
+             'happy', 'sadness', 'excite', 'anger', 'surprise', 'amaze', 'fear', 'calm', 'joyful', 'peace',
+             'mouse', 'keyboard', 'screen', 'printer', 'router', 'laptop', 'tablet', 'phone', 'speaker', 'cable',
+             'table', 'chair', 'couch', 'bed', 'lamp', 'clock', 'shelf', 'rug', 'plant', 'vase',
+             'car', 'truck', 'train', 'plane', 'bike', 'boat', 'bus', 'taxi', 'walk', 'run',
+             'dance', 'sing', 'draw', 'write', 'paint', 'play', 'read', 'cook', 'bake', 'eat',
+             'sleep', 'dream', 'laugh', 'smile', 'cry', 'talk', 'learn', 'teach', 'think',
+             'watch', 'listen', 'touch', 'smell', 'taste', 'feel', 'hear', 'see', 'say', 'do',
+             'swift', 'python', 'orange', 'black', 'white', 'green', 'blue', 'red', 'yellow', 'brown',
+             'gray', 'pink', 'every', 'than', 'this', 'while', 'was', 'that', 'some', 'place',
+             'earth', 'moon', 'sun', 'star', 'space', 'galaxy', 'nebula', 'comet', 'orbit', 'crater',
+             'heart', 'brain', 'liver', 'spleen', 'stomach', 'kidney', 'lung', 'blood', 'vein', 'nerve',
+             'sad', 'funny', 'serious', 'silly', 'calm', 'loud', 'quiet', 'fast', 'slow',
+             'hot', 'cold', 'warm', 'cool', 'dry', 'wet', 'hard', 'soft', 'sharp', 'blunt',
+             'old', 'new', 'young', 'big', 'small', 'tall', 'short', 'thick', 'thin', 'wide']
+
+## Checks word_list for dupes
+# occurrences = {}
+#
+# for item in word_list:
+#     occurrences[item] = occurrences.get(item, 0) + 1
+#
+# print(occurrences)
